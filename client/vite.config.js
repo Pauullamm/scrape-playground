b01195f84818ae9a67fc31dacda36e2907a87c37
Pauullamm <paullamsj12@gmail.com>
2025-02-07 11:49:41 +0000

    fix: obfuscation of env variables
    The error is that the contents of the env folder are obfuscated in prod

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,30 +1,42 @@
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 import obfuscatorPlugin from 'vite-plugin-javascript-obfuscator';
 
-export default defineConfig({
-  server: {
-    historyApiFallback: true
-  },
-  build: {
-    sourcemap: false
-  },
-  plugins: [
-    react(),
-    obfuscatorPlugin({
-      include: ['src/**/*.js', 'src/**/*.jsx', 'src/**/*.ts', 'src/**/*.tsx',],
-      exclude: ['src/supabaseClient/supabaseClient.js'],
-      compact: true,
-      controlFlowFlattening: true,
-      controlFlowFlatteningThreshold: 1,
-      numbersToExpressions: true,
-      simplify: true,
-      stringArrayShuffle: true,
-      splitStrings: true,
-      stringArrayThreshold: 1
-    })
-  ],
-  optimizeDeps: {
-    exclude: ['lucide-react'],
-  },
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd());
+
+  return {
+    server: {
+      historyApiFallback: true,
+    },
+    define: {
+      'import.meta.env': JSON.stringify(env), // Manually inject env variables
+    },
+    build: {
+      sourcemap: false,
+    },
+    plugins: [
+      react(),
+      mode == 'production' && obfuscatorPlugin({
+        include: ['src/**/*.js', 'src/**/*.jsx', 'src/**/*.ts', 'src/**/*.tsx'],
+        exclude: [
+          'src/supabaseClient/supabaseClient.js',
+          'vite.config.js', // Exclude Vite config
+        ],
+        compact: true,
+        controlFlowFlattening: true,
+        controlFlowFlatteningThreshold: 1,
+        numbersToExpressions: true,
+        simplify: true,
+        stringArrayShuffle: true,
+        splitStrings: true,
+        stringArrayThreshold: 1,
+        // NEW: Ignore environment variables in obfuscation
+        ignoreRequireImports: true,
+      }).filter(Boolean),
+    ],
+    optimizeDeps: {
+      exclude: ['lucide-react'],
+    },
+  };
 });
